6e2c11680969e66905ade3ece6ee4ad6496e02cf
Andrew Johnson <andrew.johnson@asurion.com>
2023-04-02 11:50:59 -0500

    package 0.0.7

--- dist/src/types.d.ts
+++ dist/src/types.d.ts
@@ -11,7 +11,7 @@ export interface EnvironmentNames {
     base: string;
     head: string | null;
 }
-declare type EnvironmentType = "master" | "feature";
+type EnvironmentType = "master" | "feature";
 export interface EnvironmentProps {
     environmentType: EnvironmentType;
     environmentNames: EnvironmentNames;
